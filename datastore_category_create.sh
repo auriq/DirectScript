d8c05b10bcb7c85ccd2b503b4fdba13f798425b9
zotsing <zotsing@gmail.com>
2016-02-11 01:32:24 +0000

    correction

--- datastore_category_create.sh
+++ datastore_category_create.sh
@@ -6,7 +6,7 @@
 ##3) how to view a compressed file -- ess lsa
 ##4) how to create a category -- ess category add
 ##5) how to modify some category attributes -- ess category change
-
+ess purge asi-essentiapublic
 #example  1
 ########select a s3 bucket as datastore########
 ess select s3://asi-essentiapublic --aws_access_key AKIAJJ2NEBGDF7I7FVZA --aws_secret_access_key ekIr5mhZHCbNNC29hW2MpzOX/oiBgJ3QOph3rxAG
@@ -22,7 +22,7 @@ ess ls climate
 #example  4
 ########list files in all subdirectories recursively########
 ess ls -r
-e
+
 #example 5
 ########create a category with default options except for pattern#####
 ess category add default 'climate'
@@ -31,14 +31,13 @@ ess category add default 'climate'
 ########create a category with a given date formatter########
 ess category add ageregex 'dateformat/Dec-31-2015_age_gender.csv' --dateregex='[:%b:]-[:%d:]-[:%Y:]'
 
-ess category add age 
 #example 8 & 9
 ########create a category with another date formatter with am/pm#######
 ess category add log 'dateformat/20140701000006am.log' --dateregex='[:%Y:][:%m:][:%d:][:%H:][:%M:][:%S:][:%p:]'
 
 #example 10 & 11
 ########create a category with preprocess command########
-ess category add climate '01*.op' --dateregex='-[:%Y:]' --preprocess='logcnv  -f,+1,eok - -d s,n=7,trm:stn s,n=7,trm:wban s,n=4,trm:year s,n=6,trm:moda s,n=7,trm:temp s,n=4,trm:Tobs s,n=7,trm:dewp s,n=4,trm:Dobs s,n=7,trm:slp s,n=4,trm:Sobs s,n=7,trm:stp s,n=4,trm:Pobs s,n=6,trm:visib s,n=4,trm:Vobs s,n=6,trm:wdsp s,n=4,trm:Wobs s,n=7,trm:mxspd s,n=7,trm:gust s,n=6,trm:max s,n=2,trm:xf s,n=6,trm:min s,n=2,trm:nf s,n=5,trm:prcp s,n=2,trm:pf s,n=7,trm:sndp s,n=6,trm:frshtt -o,notitle -'
+ess category add climate 'climate/01*.op' --dateregex='-[:%Y:]' --preprocess='logcnv  -f,+1,eok - -d s,n=7,trm:stn s,n=7,trm:wban s,n=4,trm:year s,n=6,trm:moda s,n=7,trm:temp s,n=4,trm:Tobs s,n=7,trm:dewp s,n=4,trm:Dobs s,n=7,trm:slp s,n=4,trm:Sobs s,n=7,trm:stp s,n=4,trm:Pobs s,n=6,trm:visib s,n=4,trm:Vobs s,n=6,trm:wdsp s,n=4,trm:Wobs s,n=7,trm:mxspd s,n=7,trm:gust s,n=6,trm:max s,n=2,trm:xf s,n=6,trm:min s,n=2,trm:nf s,n=5,trm:prcp s,n=2,trm:pf s,n=7,trm:sndp s,n=6,trm:frshtt -o,notitle -'
 
 #example 9
 ########overwrite a category with preprocess command#########
