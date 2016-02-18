95be456d617c8b0f76a759e254228db5b22fd046
Jing Zhou <jzhou@vela.auriq.com>
2016-02-18 14:48:12 -0800

    add cat for local

--- datastore_select.sh
+++ datastore_select.sh
@@ -2,6 +2,8 @@
 echo "select local disk"
 ess select local
 ess ls /data
+ess category add localcli 'data/climate'
+ess summary localcli
 ####select s3 datastore###############
 echo "select an s3 opendata bucket"
 ess select s3://nasanex --aws_access_key AKIAJJ2NEBGDF7I7FVZA --aws_secret_access_key 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
