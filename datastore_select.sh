50431dd80ec9414abea13e90a8fcbe05293328d2
Jing Zhou <jzhou@vela.auriq.com>
2016-02-18 14:39:41 -0800

    add description

--- datastore_select.sh
+++ datastore_select.sh
@@ -1,9 +1,12 @@
 ####select local disk as datastore#####
+echo "select local disk"
 ess select local
-ess ls
+ess ls /data
 ####select s3 datastore###############
+echo "select an s3 opendata bucket"
 ess select s3://nasanex --aws_access_key AKIAJJ2NEBGDF7I7FVZA --aws_secret_access_key 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
 ess ls
 ###select azure blob datastore#######
+echo "select an azure blob bucket"
 ess select blob://indexdev --credentials cborys_azure.csv
 ess ls
