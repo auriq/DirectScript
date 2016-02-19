a27b914a99f8433d55a0d13fcc9812814f6c3ba8
Jing Zhou <jzhou@vela.auriq.com>
2016-02-19 09:28:52 -0800

    view some opendata

--- datastore_select.sh
+++ datastore_select.sh
@@ -1,14 +1,26 @@
-####select local disk as datastore#####
-echo "select local disk"
-ess select local
-ess ls /data
-ess category add localcli 'data/climate'
-ess summary localcli
-####select s3 datastore###############
-echo "select an s3 opendata bucket"
+#####essentia helps to bring data from differnt platform to one place"
+####add s3 bucket to datastores###############
+echo "command1: add in NASA NEX large Earth science data including climate projects and satellite images"
 ess select s3://nasanex --aws_access_key AKIAJJ2NEBGDF7I7FVZA --aws_secret_access_key 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
+
+echo "command2: view NASA NEX data as if it is on a local drive"
 ess ls
+
+echo "command3: add Landsat 8 with continuous record of earth's surface from space"
+ess select s3://landsat-pds --aws_access_key AKIAJJ2NEBGDF7I7FVZA --aws_secret_access_key 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
+
+echo "command4: view NASA NEX data as if it is on a local drive"
+ess ls
+
 ###select azure blob datastore#######
-echo "select an azure blob bucket"
+
+echo "command5: add an azure blob bucket to datastore"
 ess select blob://indexdev --credentials cborys_azure.csv
+
+echo "command6: list content of the blob  root dir"
 ess ls
+
+echo "command7: switch back to s3 datastore"
+ess select nasanex   ##(equivalent to cd /path)
+
+
