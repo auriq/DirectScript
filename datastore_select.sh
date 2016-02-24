0ba0be742e5a281e72202ae7f30cf21db6baf9f3
EC2 Default User <ec2-user@ip-10-0-0-164.ec2.internal>
2016-02-24 17:08:55 +0000

    add more examples

--- datastore_select.sh
+++ datastore_select.sh
@@ -9,18 +9,21 @@ ess ls
 echo "command3: add Landsat 8 with continuous record of earth's surface from space"
 ess select s3://landsat-pds --aws_access_key AKIAJJ2NEBGDF7I7FVZA --aws_secret_access_key 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
 
-echo "command4: view NASA NEX data as if it is on a local drive"
+echo "command4: add NOAA Next Generation Weather Radar network of 160 h-d Doppler radar sites"
+ess select s3://noaa-nexrad-level2 --aws_access_key AKIAJJ2NEBGDF7I7FVZA --aws_secret_access_key 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
+
+echo "command5: view NASA NEX data as if it is on a local drive"
 ess ls
 
 ###select azure blob datastore#######
 
-echo "command5: add an azure blob bucket to datastore"
+echo "command6: add an azure blob bucket to datastore"
 ess select blob://indexdev --credentials cborys_azure.csv
 
-echo "command6: list content of the blob  root dir"
+echo "command7: list content of the blob  root dir"
 ess ls
 
-echo "command7: switch back to s3 datastore"
+echo "command8: switch back to s3 datastore"
 ess select nasanex   ##(equivalent to cd /path)
 
 
