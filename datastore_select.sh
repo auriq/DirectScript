02bc272c188c69d6eed2faff994069f047e014b7
EC2 Default User <ec2-user@ip-10-0-0-112.ec2.internal>
2016-03-08 21:38:50 +0000

    new path

--- datastore_select.sh
+++ datastore_select.sh
@@ -1,14 +1,13 @@
 ######dump credentials to the right path######
 echo "Please copy credential csv files to the following path"
-credenPath=`pwd`/.aws
-echo $credenPath
+echo $ESS_AWS_DIR
 echo " "
 echo "#######################################################"
 
 #####essentia helps to bring data from differnt platform to one place"
 ####add s3 bucket to datastores###############
 echo "command1: add in NASA NEX large Earth science data including climate projects and satellite images"
-ess select s3://nasanex --credentials $credenPath/demo_essentiaPublic.csv
+ess select s3://nasanex --credentials $ESS_AWS_DIR/demo_essentiaPublic.csv
 
 echo "command2: view NASA NEX data as if it is on a local drive"
 ess ls
@@ -19,7 +18,7 @@ echo "command4: view Landsat as if it is on a local drive"
 ess ls
 
 echo "command5: add NOAA Next Generation Weather Radar network of 160 h-d Doppler radar sites"
-ess select s3://noaa-nexrad-level2 --credentials $credenPath/demo_essentiaPublic.csv                                                      
+ess select s3://noaa-nexrad-level2 --credentials $ESS_AWS_DIR/demo_essentiaPublic.csv                                                      
 
 echo "command6: view noaa-nexrad data as if it is on a local drive"
 ess ls
