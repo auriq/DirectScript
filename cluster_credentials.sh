20e666a21c7ac716e3612d9f5741430e154b1532
EC2 Default User <ec2-user@ip-10-0-0-4.ec2.internal>
2016-03-10 19:15:04 +0000

    edit comments

--- cluster_credentials.sh
+++ cluster_credentials.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-#####if you are using credential files#######
-#####please copy the file to $ESS_AWS_DIR###
+#####if no IAM roles, using credential files#######
+#####please copy the file to $ESS_AWS_DIR##########
 ############################################
 
-printf "%s %s" "Copy credentials to" $ESS_AWS_DIR
+printf "%s %s" "#### Please copy credentials to" $ESS_AWS_DIR 
 
-echo "######setting up clusters#############"
+echo "####command1: setting up clusters##########################"
 ess cluster set cloud
 
+echo "####command2: setting up clusters using credentials########"
 ess cluster create --num=2 --credentials $ESS_AWS_DIR/credentials
 ###alternative method:###############################################
 ##ess cluster create --num=2 --aws_access_key [your_access_key] --aws_secret_access_key [your_secret key]###
 
+echo "####command3: check cluster status#########################"
 ess cluster status
 
+echo "####command4: terminate clusters###########################"
 ess cluster terminate
 
 
