22aa7f11eb2ce760964a08b615b5cce0d4210492
EC2 Default User <ec2-user@ip-10-0-0-36.ec2.internal>
2016-05-27 23:03:12 +0000

    modify

--- connect/datastore_select.sh
+++ connect/datastore_select.sh
@@ -1,13 +1,9 @@
-######dump credentials to the right path######
-echo "Please copy credential csv files to the following path"
-echo $ESS_AWS_DIR
-echo " "
 echo "#######################################################"
 
 #####essentia helps to bring data from differnt platform to one place"
 ####add s3 bucket to datastores###############
 echo "command1: add in NASA NEX large Earth science data including climate projects and satellite images"
-ess select s3://nasanex --credentials $ESS_AWS_DIR/demo_essentiaPublic.csv
+ess select s3://nasanex 
 
 echo "command2: view NASA NEX data as if it is on a local drive"
 ess ls
@@ -18,7 +14,7 @@ echo "command4: view Landsat as if it is on a local drive"
 ess ls
 
 echo "command5: add NOAA Next Generation Weather Radar network of 160 h-d Doppler radar sites"
-ess select s3://noaa-nexrad-level2 --credentials $ESS_AWS_DIR/demo_essentiaPublic.csv                                                      
+ess select s3://noaa-nexrad-level2 
 
 echo "command6: view noaa-nexrad data as if it is on a local drive"
 ess ls
@@ -29,4 +25,5 @@ ess select nasanex   ##(equivalent to cd /path)
 echo "command8: view NASA NEX data as if it is on a local drive"
 ess ls
 
-
+echo "command9: connect to auriq opendata"
+ess select s3://asi-essentiapublic
